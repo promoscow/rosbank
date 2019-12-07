1f445bcb2826bdb42f40e92e0d9c795bb8156517
nemchinovrp <nemchinovrp@yandex.ru>
2019-12-07 18:52:43 +0300

    Вынес логику из контроллера

--- src/main/java/ru/xpendence/rosbank/controller/AgentRequestController.java
+++ src/main/java/ru/xpendence/rosbank/controller/AgentRequestController.java
@@ -2,13 +2,9 @@ package ru.xpendence.rosbank.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
 import ru.xpendence.rosbank.dto.AgentRequest;
 import ru.xpendence.rosbank.dto.ResponseEnd;
-import ru.xpendence.rosbank.dto.TaxServiceResponseDto;
 import ru.xpendence.rosbank.service.FindBranchService;
 
 @CrossOrigin
@@ -16,20 +12,10 @@ import ru.xpendence.rosbank.service.FindBranchService;
 @RequestMapping("/agent")
 @RequiredArgsConstructor(onConstructor_ = {@Autowired})
 public class AgentRequestController {
-    private final RestTemplate restTemplate;
     private final FindBranchService findBranchService;
 
     @PostMapping("/request")
     public ResponseEnd getInfoFromAgent(@RequestBody AgentRequest agentRequest) {
-        String url = "http://algo:8082/inn";
-
-        ResponseEntity<TaxServiceResponseDto> taxServiceResponse = restTemplate.getForEntity(
-                UriComponentsBuilder.fromHttpUrl(url).queryParam("inn", agentRequest.getInnNumber()).toUriString(),
-                TaxServiceResponseDto.class);
-        TaxServiceResponseDto resDto = taxServiceResponse.getBody();
-
-        return findBranchService.getBranches(Double.parseDouble(resDto.getLatitude()),
-                Double.parseDouble(resDto.getLongitude()),
-                resDto.getRegion());
+        return findBranchService.sendRequest(agentRequest);
     }
 }
--- src/main/java/ru/xpendence/rosbank/service/FindBranchService.java
+++ src/main/java/ru/xpendence/rosbank/service/FindBranchService.java
@@ -2,10 +2,15 @@ package ru.xpendence.rosbank.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 import ru.xpendence.rosbank.dao.BranchRepository;
 import ru.xpendence.rosbank.dao.CityRepository;
+import ru.xpendence.rosbank.dto.AgentRequest;
 import ru.xpendence.rosbank.dto.ResponseEnd;
+import ru.xpendence.rosbank.dto.TaxServiceResponseDto;
 import ru.xpendence.rosbank.model.Branch;
 import ru.xpendence.rosbank.model.City;
 
@@ -16,6 +21,7 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor(onConstructor_ = {@Autowired})
 public class FindBranchService {
+    private final RestTemplate restTemplate;
     private final CityRepository cityRepository;
     private final BranchRepository branchRepository;
 
@@ -32,6 +38,19 @@ public class FindBranchService {
                 .limit(1)
                 .collect(Collectors.toList());
         return new ResponseEnd(latitude, longitude, cityName, resultBranch);
+    }
+
+    public ResponseEnd sendRequest(AgentRequest agentRequest) {
+        String url = "http://algo:8082/inn";
+
+        ResponseEntity<TaxServiceResponseDto> taxServiceResponse = restTemplate.getForEntity(
+                UriComponentsBuilder.fromHttpUrl(url).queryParam("inn", agentRequest.getInnNumber()).toUriString(),
+                TaxServiceResponseDto.class);
+        TaxServiceResponseDto resDto = taxServiceResponse.getBody();
+
+        return getBranches(Double.parseDouble(resDto.getLatitude()),
+                Double.parseDouble(resDto.getLongitude()),
+                resDto.getRegion());
     }
 
     public String getCity(String comment) {
